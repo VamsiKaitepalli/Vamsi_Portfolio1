c1205449c956f5a35a9439391da7e3951e2481d5
VAMSI <143600477+VamsiKaitepalli@users.noreply.github.com>
2023-10-24 11:25:26 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 
+window.addEventListener('load', function () {
+    var card = document.querySelector('.section');
+    card.classList.add('slide-in');
+
+});
+
+// FOR SLIDING ANIMATION
+
+// ScrollReveal().reveal('.slide-in', {
+//     distance: '20%',
+//     origin: 'bottom',
+//     opacity: 0,
+//     easing: 'ease-in-out',
+//     duration: 1000,
+//     interval: 200,
+//     reset:true
+
+// });
